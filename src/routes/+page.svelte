7d63c4f9f3bfabd520309df7c20167a5f2d7b447
Fabian Valka <fvalka@vektorraum.com>
2025-11-26 22:32:26 +0000

    Intial prototype

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<div class="border w-200 m-4 p-2">
+    <h1>Cold Temperature Altitude Correction</h1>
+
+    <div>
+        <h2>Aerodrome</h2>
+        <div class="grid grid-cols-2 gap-1 m-2 w-200">
+            <label>
+                <div>Elevation of the Aerodrome</div>
+                <input type="number" class="border rounded p-2">
+            </label>
+
+            <label>
+                <div>Temperature at the Aerodrome</div>
+                <input type="number" class="border rounded p-2">
+            </label>
+        </div>
+    </div>
+</div>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
