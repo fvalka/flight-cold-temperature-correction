338b89bbdcf63ca20ec905c6a7c0a469d8e7a3d0
Fabian Valka <fvalka@vektorraum.com>
2025-11-29 18:47:12 +0000

    Improved display of input errors for altitude corrections on the UI

--- src/lib/components/AltitudeCorrection.svelte
+++ src/lib/components/AltitudeCorrection.svelte
@@ -9,12 +9,44 @@
     label,
     aerodrome_elevation_ft,
     aerodrome_ground_temperature_degC,
-    isInputValid,
+    isInputValid: is_output_enabled,
     input_altitude_ft = $bindable(undefined),
   } = $props();
 
+  let inputErrors: string[] = $state([]);
+  let is_input_valid = $state(false);
+
+  function validateInput() {
+        console.log("validating input!");
+        inputErrors = [];
+
+        // Short circuiting errors 
+        if (!isValidNumber(input_altitude_ft)) {
+          is_input_valid = false;
+          return;
+        }
+        if(input_altitude_ft < aerodrome_elevation_ft) {
+          is_input_valid = false;
+          return;
+        }
+
+        if (input_altitude_ft > 36000) {
+            inputErrors.push("Please input a valid altitude of 36000ft or less!");
+        }
+
+        if (aerodrome_elevation_ft < -1000) {
+            inputErrors.push("Please enter a valid altitude!")
+        }
+
+        is_input_valid = inputErrors.length == 0;
+
+    }
 
   function altitudeCorrectionFormatted(input_altitude_ft: any) {
+    if(!is_input_valid) {
+      return undefined;
+    }
+
     if (
       !isValidNumber(input_altitude_ft) ||
       !isValidNumber(aerodrome_elevation_ft) ||
@@ -39,14 +71,27 @@
   }
 </script>
 
-<NumericalInputField {label} bind:value={input_altitude_ft} unit="ft"
+<NumericalInputField {label} 
+  bind:value={input_altitude_ft} 
+  unit="ft"
+  oninput={ validateInput }
 ></NumericalInputField>
 <NumericalOutputLabel
   label={label == "" ? "" : "Corrected " + label}
   value={altitudeCorrectionFormatted(input_altitude_ft)}
   unit="ft"
-  isInputValid={ isInputValid }
+  isInputValid={ is_output_enabled && is_input_valid }
 ></NumericalOutputLabel>
+
+
+{#each inputErrors as inputErrorMessage} 
+    <div
+        class="text-error-700-300 border-2 border-error-300-700 col-span-2 rounded p-1"
+    >
+        { inputErrorMessage }
+    </div>
+{/each}
+
 
 <style lang="postcss">
   @reference "tailwindcss";
